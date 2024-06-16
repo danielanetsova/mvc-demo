a74a9779d4977083022c3d8dca72283cc28bd982
Daniela Netsova <netsovadaniela@gmail.com>
2024-06-16 22:15:41 +0300

    Added HomeController and UsersController.
    HomeController has following methods:
    - index method which maps HTTP GET requests to the root URL and returns the ModelAndView object to the dispatcher servlet, which then resolves and renders the view.
    - home method which maps HTTP GET requests to the "/home" URL, accepts an optional request parameter "show-ad" and returns the view name "home".
    
    UsersController has following methods:
    - login method which maps HTTP GET requests to the "/login" URL and returns the view name "user/login".
    - doLogin method which handles POST requests to the /login URL. The method accepts two parameters @Valid UserLoginDto loginDto(binds the request body to a UserLoginDto
    object and applies validation constraints defined in the UserLoginDto class) and BindingResult bindingResult(captures the results of the validation and binding. It holds validation errors, if any).
    The method checks if the username is "admin" and password is "secure" and if both conditions are met, it considers the login successful.
    If the login is successful, it creates a ModelAndView object and sets the view name to "redirect:/home", which will redirect the user to the "/home" URL. If the login credentials are incorrect,
    it collects validation errors from the BindingResult object.  The errors are mapped to a list of strings each containing the object name and the default message. If both conditions above
    are not met, a new ModelAndView object is created with the view name set to "user/login". The errors list is added to the model under the key "errors".
    Finally, the method returns the ModelAndView object, which will render the login page with the error messages.
    - register method which maps HTTP GET requests to the "/register" URL and returns the view name "user/register".
    - doRegister method which maps HTTP POST requests to the "/register" URL. Accepts @Valid UserLoginDto loginDto which binds the request body to a UserLoginDto object
    and applies validation constraints defined in the UserLoginDto class. The UserService register method is called  which returns boolean representing whether the registration is successful.
    If so, returns the view name "redirect: /users/login" which which will redirect the user to the "/users/login" URL, else returns the view name "user/register".
    - getUserInfo method which maps HTTP GET requests to the URL pattern "/info/{id}". {id} is a path variable, meaning the value in this part of the URL will be captured
    and passed to the method. @PathVariable("id") long id: This annotation binds the path variable {id} from the URL to the method parameter id.
    The id is then available as a long type within the method. eturns the view name "home".

--- src/main/java/bg/softuni/MVCDemo/controllers/HomeController.java
+++ src/main/java/bg/softuni/MVCDemo/controllers/HomeController.java
@@ -2,6 +2,7 @@ package bg.softuni.MVCDemo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,7 +17,7 @@ public class HomeController {
     }
 
     @GetMapping("/home")
-    public String home() {
+    public String home(@RequestParam(value = "show-ad", required = false) Integer adId) {
         return "home";
     }
 }
--- src/main/java/bg/softuni/MVCDemo/controllers/UsersController.java
+++ src/main/java/bg/softuni/MVCDemo/controllers/UsersController.java
@@ -1,13 +1,73 @@
 package bg.softuni.MVCDemo.controllers;
 
+import bg.softuni.MVCDemo.dtos.UserLoginDto;
+import bg.softuni.MVCDemo.dtos.UserRegisterDto;
+import bg.softuni.MVCDemo.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
+@RequestMapping("/users")
 public class UsersController {
-    @GetMapping("/users/login")
+    private final UserService userService;
+
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/login")
     public String login() {
-        
+        return "user/login";
+    }
+
+    @PostMapping("/login")
+    public ModelAndView doLogin(@Valid UserLoginDto loginDto, BindingResult bindingResult) {
+        if (loginDto.getUsername().equals("admin") && loginDto.getPassword().equals("secure")) {
+
+            ModelAndView result = new ModelAndView();
+            result.setViewName("redirect:/home");
+            return result;
+        }
+
+        List<String> errors = bindingResult
+                .getAllErrors()
+                .stream()
+                .map(error -> error.getObjectName() + " " + error.getDefaultMessage())
+                .toList();
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("user/login");
+        modelAndView.addObject("errors", errors);
+
+        return modelAndView;
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "user/register";
+    }
+
+    @PostMapping("/register")
+    public String doRegister(@Valid UserRegisterDto registerDto) {
+        boolean successfulRegistration = userService.register(registerDto);
+
+        if (successfulRegistration) {
+            return "redirect: /users/login";
+        }
+
+        return "user/register";
+    }
+
+    @GetMapping("/info/{id}")
+    public String getUserInfo(@PathVariable("id") long id) {
+        return "home";
     }
 }
