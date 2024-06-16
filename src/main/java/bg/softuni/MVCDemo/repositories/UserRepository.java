a5bc01c573a5850e2a1d5d39b5baf05a951a1968
Daniela Netsova <netsovadaniela@gmail.com>
2024-06-16 22:10:31 +0300

    Added UserRepository and UserService. - UserRepository has an 'existsByEmail' method which accepts a String email and returns a boolean indicating whether a user with this email is present in the database. - UserService has a register method which accepts a UserRegisterDto registerDto. Inside the method, the 'existsByEmail' method of UserRepository is called with registerDto's email as a parameter. If the email from registerDto is present in the database, the register method in UserService returns false, indicating that registration was not successful. Otherwise, a new User is inserted into the database.

---
+++ src/main/java/bg/softuni/MVCDemo/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package bg.softuni.MVCDemo.repositories;
+
+import bg.softuni.MVCDemo.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+}
---
+++ src/main/java/bg/softuni/MVCDemo/services/UserService.java
@@ -0,0 +1,28 @@
+package bg.softuni.MVCDemo.services;
+
+import bg.softuni.MVCDemo.dtos.UserRegisterDto;
+import bg.softuni.MVCDemo.entities.User;
+import bg.softuni.MVCDemo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public boolean register(UserRegisterDto registerDto) {
+        boolean exists = userRepository.existsByEmail(registerDto.getEmail());
+
+        if (exists) return false;
+
+        User user = new User(registerDto);
+        this.userRepository.saveAndFlush(user);
+
+        return true;
+    }
+}
