6a67f8c262a6d10b375e20c2e08d977e4aab9a23
Daniela Netsova <netsovadaniela@gmail.com>
2024-06-27 10:48:56 +0300

    Added new methods in UserRepository and UserService
    - In UserRepository 'findByUsername' method has been addded.
    - In UserService  a new method 'login' has been added. It accepts 'UserLoginDto' object,
    verifies if the user exists in the database and checks if the passwords match.

--- src/main/java/bg/softuni/MVCDemo/repositories/UserRepository.java
+++ src/main/java/bg/softuni/MVCDemo/repositories/UserRepository.java
@@ -4,7 +4,10 @@ import bg.softuni.MVCDemo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+    Optional<User> findByUsername(String username);
 }
--- src/main/java/bg/softuni/MVCDemo/services/UserService.java
+++ src/main/java/bg/softuni/MVCDemo/services/UserService.java
@@ -1,10 +1,13 @@
 package bg.softuni.MVCDemo.services;
 
+import bg.softuni.MVCDemo.dtos.UserLoginDto;
 import bg.softuni.MVCDemo.dtos.UserRegisterDto;
 import bg.softuni.MVCDemo.entities.User;
 import bg.softuni.MVCDemo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -18,11 +21,20 @@ public class UserService {
     public boolean register(UserRegisterDto registerDto) {
         boolean exists = userRepository.existsByEmail(registerDto.getEmail());
 
-        if (exists) return false;
+        if (exists || !registerDto.getConfirmPassword().equals(registerDto.getPassword())) return false;
 
         User user = new User(registerDto);
         this.userRepository.saveAndFlush(user);
 
         return true;
+    }
+
+    public boolean login(UserLoginDto loginDto) {
+        Optional<User> optionalUser = userRepository.findByUsername(loginDto.getUsername());
+
+        return optionalUser.isPresent() &&
+                optionalUser.get()
+                        .getPassword()
+                        .equals(loginDto.getPassword());
     }
 }
